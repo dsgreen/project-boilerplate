c4e32c8f8d7c965f357f414c5d253418301b5ce1
Doug Green <doug@dsgreen.com>
2018-05-31 17:12:13 -0400

    Move scripts to document ready

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -3,23 +3,24 @@
  */
 
 // 1st page load
-$(window).load(function() {
+jQuery(document).ready(function($) {
   if (window.innerWidth < 768) {
     $('.main-navigation').hide();
   }
-});
 
-// toggle button
-$('.nav-toggle').click(function(){
-  $('.main-navigation').slideToggle('fast');
-});
+  // toggle button
+  $('.nav-toggle').click(function(){
+    $('.main-navigation').slideToggle('fast');
+  });
 
-// browser resize, collapse mobile menu
-$(window).resize(function() {
-  if (window.innerWidth > 767) {
-    $('.main-navigation').show();
-  }
-  else if (window.innerWidth < 768) {
-    $('.main-navigation').hide();
-  }
+  // browser resize, collapse mobile menu
+  $(window).resize(function() {
+    if (window.innerWidth > 767) {
+      $('.main-navigation').show();
+    }
+    else if (window.innerWidth < 768) {
+      $('.main-navigation').hide();
+    }
+  });
+
 });
--- src/js/scrollto.js
+++ src/js/scrollto.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function($) {
 
   /*
    * back to top button
